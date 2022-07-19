fbde381a70ac8b88fc25c594ff9d8713a31a1756
tkuchi <92394716+tkuchi@users.noreply.github.com>
2022-07-19 11:09:27 -0400

    Initial Commit

---
+++ To-Do List Project/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  To-Do List Project
+//
+//  Created by Tanishqa Kuchi on 7/19/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name : ""
+    var important : false
+}
+
+
+
+
+
